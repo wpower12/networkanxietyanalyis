5d53605dfb0bc68785952ff36a9ca5d7fe38f041
wpower12 <willpowe@gmail.com>
2022-08-17 08:59:44 -0400

    Updated huts gather. To get mentioned users.

--- 20_draft_huts_w_mention_nets.py
+++ 20_draft_huts_w_mention_nets.py
@@ -5,12 +5,13 @@ from tqdm import tqdm
 import snscrape.modules.twitter as sntwitter
 
 DATE_RANGE = ['2022-01-02', '2022-06-20']
-FN_USERS = "data/raw/mil_bases/mil_base_usernames.csv"
-FN_OUT = "data/raw/mil_bases/historic_user_tweets_w_mn.csv"
-FN_PROC_USERS = "data/raw/mil_bases/processed_users_mn.csv"
+FN_USERS = "data/prepared/mb_mentioned_users_RIGHTCOLS.csv"
+FN_OUT = "data/prepared/mb_raw_mentioned_tweets.csv"
+FN_PROC_USERS = "data/raw/processed_users.csv"
 
-TIMEOUT_LEN_S = 15
+TIMEOUT_LEN_S = 5
 MAX_ATTEMPTS = 1
+MAX_RESULTS = 1000
 
 # All features are numbers (integers, even)
 FEATURES = ["u_acct_age",
@@ -58,46 +59,57 @@ def get_tweet_features(tweet):
             t_n_retweets, t_n_hashtags, t_n_user_mentions, t_n_urls, t_n_chars, t_n_digits]
 
 
-user_df = pd.read_csv(FN_USERS)
-proc_users = pd.read_csv(FN_PROC_USERS, header=None, dtype={0: int})[0].tolist()
+user_df = pd.read_csv(FN_USERS, dtype={0: int}, index_col=0)
+proc_users = pd.read_csv(FN_PROC_USERS, header=None, dtype={0: int})[0].to_list()
+user_df.drop(proc_users, inplace=True)
 
+users_skipped = 0
+tweets_mined = 0
 n_users = len(user_df)
-c_users = 0
-for _, row in tqdm(user_df.iterrows(), total=n_users):
-    u_id, _, u_name = row
 
-    if u_id in proc_users:
-        continue
+user_pbar = tqdm(user_df.iterrows(), total=n_users, unit="user")
+for u_id, row in user_pbar:
+    _, u_name = row
 
+    # sns_query = "from:{} since:{} until:{} max:{}".format(u_name, DATE_RANGE[0], DATE_RANGE[1], MAX_RESULTS)
     sns_query = "from:{} since:{} until:{}".format(u_name, DATE_RANGE[0], DATE_RANGE[1])
-    successful = False
     attempts = 0
-    while not successful and attempts < MAX_ATTEMPTS:
-        try:
-            user_tweets = []
-            for i, tweet in enumerate(sntwitter.TwitterSearchScraper(sns_query).get_items()):
-                features = get_tweet_features(tweet)
-                if tweet.mentionedUsers is not None:
-                    mentions = [u.id for u in tweet.mentionedUsers]
-                else:
-                    mentions = []
-                row = [tweet.date, u_id, tweet.id, tweet.content, mentions]
-                row.extend(features)
-                user_tweets.append(row)
+    # Fuck it. just one attempt per user.
+    try:
+        user_tweets = []
+        sns_res = sntwitter.TwitterSearchScraper(sns_query).get_items()
+        n = 0
+        user_pbar.postfix = "{:3d}/{:3d} max user tweets".format(n, MAX_RESULTS)
+        for tweet in sns_res:
+            features = get_tweet_features(tweet)
+            if tweet.mentionedUsers is not None:
+                mentions = [u.id for u in tweet.mentionedUsers]
+            else:
+                mentions = []
+            row = [tweet.date, u_id, tweet.id, tweet.content, mentions]
+            row.extend(features)
+            user_tweets.append(row)
+            user_pbar.postfix = "{:3d}/{:3d} max user tweets".format(n, MAX_RESULTS)
+            n += 1
 
-            successful = True
-            user_df = pd.DataFrame(user_tweets, columns=DF_COLUMNS)
-            user_df.to_csv(FN_OUT, mode='a', index=False, quoting=csv.QUOTE_NONNUMERIC, header=False)
+            if n > MAX_RESULTS:
+                break
 
-            with open(FN_PROC_USERS, 'a') as f_pu:
-                f_pu.write("{}\n".format(u_id))
+        user_df = pd.DataFrame(user_tweets, columns=DF_COLUMNS)
+        user_df.to_csv(FN_OUT, mode='a', index=False, quoting=csv.QUOTE_NONNUMERIC, header=False)
 
-        except KeyboardInterrupt:
-            raise
+        tweets_mined += len(user_tweets)
+        user_pbar.desc = "users skipped: {} | tweets mined: {}".format(users_skipped, tweets_mined)
 
-        except Exception as e:
-            print("caught: {}".format(e))
-            print("waiting and retrying")
-            time.sleep(TIMEOUT_LEN_S)
-            attempts += 1
+        with open(FN_PROC_USERS, 'a') as f_pu:
+            f_pu.write("{}\n".format(u_id))
 
+    except KeyboardInterrupt:
+        raise
+
+    # except Exception as e:
+    #     users_skipped += 1
+    #     user_pbar.desc = "users skipped: {} | tweets mined: {}".format(users_skipped, tweets_mined)
+    #     user_pbar.postfix(" | PAUSED - timeout")
+    #     time.sleep(TIMEOUT_LEN_S)
+    #     user_pbar.postfix("")
