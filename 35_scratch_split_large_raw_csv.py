529e5ac5bd969c9365259b5f611e7352672e998c
wpower12 <willpowe@gmail.com>
2022-08-26 15:33:57 -0400

    updated full pipeline with more reporting and auto-creation of output dirs.

---
+++ 35_scratch_split_large_raw_csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from twitter_utils import utils
+
+FN_INPUT_SMALL = "data/raw/mb_raw_mentioned_tweets_POS_EXAMPLES.csv"
+FN_INPUT_LARGE = "data/mb_raw_mentioned_tweets_POS_EXAMPLES.csv"
+
+df = utils.load_huts_mn_df_from_fn(FN_INPUT_LARGE)
+
+print(len(df))
